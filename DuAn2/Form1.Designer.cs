5a8ac25902364219682efe037dd12ecef62204fd
puarudz <phongpt.15th.0005@sv.daihoclongan.edu.vn>
2019-03-22 15:34:04 +0700

    Update Form1.Designer.cs

--- DuAn2/Form1.Designer.cs
+++ DuAn2/Form1.Designer.cs
@@ -183,7 +183,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(183, 202);
+            this.ClientSize = new System.Drawing.Size(183, 180);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.tru);
